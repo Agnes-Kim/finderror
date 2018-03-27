92c8eab669f6fe5238db63ec60b09300a7fa077d
Agnes-Kim <agnes9506@gmail.com>
2018-03-27 23:55:33 +0900

    Update 0327_url_orginal.py
    get report number, body and title of all closed bug report
    (+encoding problem solved)

--- 0327_url_orginal.py
+++ 0327_url_orginal.py
@@ -1,18 +1,30 @@
 import urllib.request 
 import json 
 import datetime 
+import codecs
 
- 
+bugreport = 0
+f=codecs.open("lol.txt","w","utf-8")
+#url = 'https://api.github.com/repos/bitcoin/bitcoin/issues?state=closed&labels=bug&page=10'
+for x in range (1,20):
+    url='https://api.github.com/repos/bitcoin/bitcoin/issues?state=closed&labels=bug&page=%s' %x
+    u = urllib.request.urlopen(url) 
+    data = u.read() 
 
-url = 'https://api.github.com/repos/bitcoin/bitcoin/issues?state=closed&labels=bug&page=10' 
+    j = json.loads(data)
 
-u = urllib.request.urlopen(url) 
+    #print(j[0]['labels'][0]['id'])
 
-data = u.read() 
- 
+    i=0
+    for item in j:      
+        if(j[i]['number']): #bug report number
+            f.write("Report number: %s\n" %j[i]['number'])
+        if(j[i]['title']):
+            f.write("title: %s\n" %j[i]['title'])
+        if(j[i]['body']):
+            f.write("body: %s\n" %j[i]['body'])
+        i+=1
+        f.write("\n")
+    x+=1
 
-j = json.loads(data) 
-
-#print (json.dumps(j, indent=4, sort_keys=True))
- 
-print(j[1]['labels'][0]['id'])
+f.close()
