095c3c81bafafb63cf3407fb2d4989010e931caa
Agnes-Kim <agnes9506@gmail.com>
2018-04-04 18:18:56 +0900

    issue만
    pull인 것은 제외시키고 저장

--- 0327_body,title,num.py
+++ 0327_body,title,num.py
@@ -6,6 +6,7 @@ import codecs
 bugreport = 0
 f=codecs.open("lol.txt","w","utf-8")
 #url = 'https://api.github.com/repos/bitcoin/bitcoin/issues?state=closed&labels=bug&page=10'
+
 for x in range (1,20):
     url='https://api.github.com/repos/bitcoin/bitcoin/issues?state=closed&labels=bug&page=%s' %x
     u = urllib.request.urlopen(url) 
@@ -16,12 +17,13 @@ for x in range (1,20):
     #print(j[0]['labels'][0]['id'])
 
     i=0
-    for item in j:      
-        f.write("Reportkyk kyknumber: %s\n" %j[i]['number'])
-        f.write("kyktitle: %s\n" %j[i]['title'])
-        f.write("kykbody: %s\n" %j[i]['body'])
+    for item in j:
+        html_url=j[i]['html_url']
+        if "issues" in html_url:
+            f.write("Reportkyk kyknumber: %s\n" %j[i]['number'])
+            f.write("kyktitle: %s\n" %j[i]['title'])
+            f.write("kykbody: %s\n" %j[i]['body'])
         i+=1
         f.write("\n")
-    x+=1
-
+  
 f.close()
