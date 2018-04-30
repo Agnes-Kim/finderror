519b405d49267a13cc200ad1526aa9d5ef2f6779
Agnes-Kim <agnes9506@gmail.com>
2018-05-01 00:44:25 +0900

    0501

--- tq/try/src/trytry/ShowCaseParser.java
+++ tq/try/src/trytry/ShowCaseParser.java
@@ -1,7 +1,7 @@
 package trytry;
 
 import java.io.BufferedReader;
-//import java.io.BufferWriter;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
@@ -28,6 +28,7 @@ import org.jsoup.select.Elements;
 public class ShowCaseParser {
 	static String gitUrl = "https://github.com/";
 	static String urlSuffix = "?utf8=&q=is%3Aissue+is%3Aclosed+label%3A";
+
 	
 	static String apiUrl = "https://api.github.com/repos/";
 	static String apiSuffix = "?state=closed&labels=bug";
@@ -51,6 +52,8 @@ public class ShowCaseParser {
 		
 		for(int i = 0 ; i<targetOssArray.size(); i++){
 			String targetName = targetOssArray.get(i).replace("/issues", "");
+
+			System.out.println(targetOssArray.get(i).replace("/issues", "").replaceAll("/", "-"));
 			System.out.println("[CURRENT STATE] "+format.format((i+1.0)/targetOssArray.size()*100)+"%  "+targetName);
 						
 			Document doc;		
@@ -66,7 +69,7 @@ public class ShowCaseParser {
 				System.err.println("CONNECTION TIMED OUT Socket - 109");
 				continue;
 			}
-			
+
 			int lastID = Integer.parseInt(String.valueOf(doc.select("div.float-left.col-9.p-2.lh-condensed").get(0).getElementsByAttribute("href")).split("\"")[1].split("issues/")[1]);
 			
 			try{
@@ -80,6 +83,7 @@ public class ShowCaseParser {
 				System.err.println("CONNECTION TIMED OUT Socket - 109");
 				continue;
 			}
+			
 			int startID = Integer.parseInt(String.valueOf(doc.select("div.float-left.col-9.p-2.lh-condensed").get(0).getElementsByAttribute("href")).split("\"")[1].split("issues/")[1]);
 						
 			System.out.println(startID + " " +lastID);
@@ -88,9 +92,20 @@ public class ShowCaseParser {
 			HashMap<Integer, ArrayList<String>> issueCommitMap = new HashMap<Integer, ArrayList<String>>();
 			
 			for(int j = startID; j<=lastID; j++){
+				String rfileName = "C:\\Users\\skdisk\\"+targetOssArray.get(i).replace("/issues", "").replaceAll("/", "_")+"_rfile.txt" ;
+				String ifileName = "C:\\Users\\skdisk\\"+targetOssArray.get(i).replace("/issues", "").replaceAll("/", "_")+"_issues.txt" ;
+				String ibodyfileName = "C:\\Users\\skdisk\\"+targetOssArray.get(i).replace("/issues", "").replaceAll("/", "_")+"_issuesbody.txt" ;
+				String commentsfile="C:\\Users\\skdisk\\"+targetOssArray.get(i).replace("/issues", "").replaceAll("/", "_")+"_comments.txt" ;
+				
+				System.out.println(j);
 				Document doc2 = null;
+				Document docpull = null;
+				if(j==296) {
+					j++; //no 296 page, skip
+				}
 				String key = targetOssArray.get(i).replace("/issues", "");
 				String url = gitUrl+targetOssArray.get(i)+"/"+j+"\t";
+				
 				try{
 					doc2 = Jsoup.connect(url).maxBodySize(0).timeout(10000).ignoreContentType(true).get();				
 				}catch(ConnectException e){
@@ -101,7 +116,98 @@ public class ShowCaseParser {
 					continue;
 				}
 				String title = String.valueOf(doc2.getElementsByTag("title")).toLowerCase();
+
+				//issues 
+				String temptitle = String.valueOf(doc2.select("span.js-issue-title"));
+				String issuetitle= temptitle.split("</")[0].split(">")[1];
+				String tempopendate=String.valueOf(doc2.select("div.TableObject-item.TableObject-item--primary").select("relative-time"));
+				String opendate=null;
+				String closedate=null;
+				String body=null;
+				try {
+					opendate=tempopendate.split("T")[0].split("\"")[1];
+				}
+				catch(ArrayIndexOutOfBoundsException exception) {
+					//case: ~~ wants to merge 1 commit into ~~ form ~~
+				    continue;
+				}
+				
+				String tempclosedate=String.valueOf(doc2.select("div.discussion-item.discussion-item-closed").select("relative-time"));
+				String noclosedate=String.valueOf(doc2.select("div.timeline-comment-header.clearfix").select("relative-time"));
+				Elements elementdate=doc2.select("div.timeline-comment-header.clearfix").select("relative-time");
+				int sizeof=elementdate.size();
+				try {
+					closedate=tempclosedate.split("T")[0].split("\"")[1];
+				}catch(ArrayIndexOutOfBoundsException exception){
+					noclosedate=String.valueOf(elementdate.get(sizeof-1));
+					closedate=noclosedate.split("T")[0].split("\"")[1]; //not pull, no close date
+				}
+				//pull
+				if(title.contains("pull request #"+j)) {
+					closedate=opendate;
+					noclosedate=String.valueOf(elementdate.get(0));
+					opendate=noclosedate.split("T")[0].split("\"")[1];
+				}
+				BufferedWriter fw = new BufferedWriter(new FileWriter(ifileName, true));
+				fw.write("(("+j+")) "+issuetitle+" ((open)) "+opendate+"  ((close)) "+closedate);
+				fw.newLine();
+				fw.flush();
+				fw.close();
+				
+				//body
+				Elements tempbody= doc2.select("td.d-block.comment-body.markdown-body.js-comment-body");
+				body=String.valueOf(tempbody.get(0)).split("<td class=\"d-block comment-body markdown-body  js-comment-body\">")[1];
+				BufferedWriter bodyw = new BufferedWriter(new FileWriter(ibodyfileName, true));
+				bodyw.write("(("+j+"))  "+body);
+				bodyw.newLine();
+				bodyw.flush();
+				bodyw.close();
+				
+				//comments
+				String cdate=null;
+				String cbody=null;
+				String userid=null;
+				Elements celements=doc2.select("div.timeline-comment-wrapper");
+				
+				for(int c=1;c<celements.size();c++) {
+					BufferedWriter commentw = new BufferedWriter(new FileWriter(commentsfile, true));
+					userid=String.valueOf(celements.get(c).select("a.author.text-inherit")).split(">")[1].split("<")[0];
+					cdate=String.valueOf(celements.get(c).select("relative-time")).split("\"")[1].split("T")[0];
+					cbody=String.valueOf(celements.get(c).select("td.d-block.comment-body.markdown-body.js-comment-body")).split("<td class=\"d-block comment-body markdown-body  js-comment-body\">")[1];
+					System.out.println(cbody);
+					commentw.write("(("+j+")) "+userid+"  ((date)) "+cdate+ "  ((body)) "+cbody);
+					commentw.newLine();
+					commentw.flush();
+					commentw.close();
+				}
+								
+				
+				
+				
+				//pull request's revised files
+				String pullurl=gitUrl+targetName+"/pull/"+j+"/files\t";
 				if(title.contains("pull request #"+j)){
+					System.out.println(pullurl);
+					try{
+						docpull = Jsoup.connect(pullurl).maxBodySize(0).timeout(10000).ignoreContentType(true).get();				
+					}catch(ConnectException e){
+						System.err.println("ERROR TIME CONNECT");
+						continue;
+					}catch( SocketTimeoutException e2){
+						System.err.println("errot socket timeout");
+						continue;
+					}
+					String rfilen = String.valueOf(docpull.select("a.link-gray-dark"));
+					Elements element=docpull.select("a.link-gray-dark");
+					
+					for(int z = 0; z<element.size();z++) {
+						String filen=element.get(z).attr("title");
+						BufferedWriter fw2 = new BufferedWriter(new FileWriter(rfileName, true));
+						fw2.write(j+" "+filen);
+						fw2.newLine();
+						fw2.flush();
+						fw2.close();
+					}
 					continue;
 				}
 				
@@ -120,8 +226,21 @@ public class ShowCaseParser {
 							ArrayList<Integer> data = new ArrayList<Integer>();
 							data.add(pullID);
 							issuePullMap.put(j, data);							
-						}					
-						System.out.println(j+" "+k+" [PULL] "+linkURL+" "+pullID);	
+						}	
+						/*
+						String fileName = "C:\\Users\\skdisk\\pullparse.txt" ;
+						try {
+
+							BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, true));
+							fw.write(j+" "+k+" [PULL] "+linkURL+" "+pullID );
+							fw.newLine();
+							fw.flush();
+							fw.close();
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+						*/
+						//System.out.println(j+" "+k+" [PULL] "+linkURL+" "+pullID);//이거를 파일에	
 					}
 				}
 				
@@ -138,17 +257,30 @@ public class ShowCaseParser {
 						ArrayList<String> data = new ArrayList<String>();
 						data.add(commitID);
 						issueCommitMap.put(j, data);							
-					}		
-					System.out.println(j+" "+k+" [COMMIT] "+linkURL);
+					}	
+					/*
+					String fileName = "C:\\Users\\skdisk\\commitparse.txt" ;
+					try {
+
+						BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, true));
+						fw.write(j+" "+k+" [COMMIT] "+linkURL);
+						fw.newLine();
+						fw.flush();
+						fw.close();
+					}catch(Exception e){
+						e.printStackTrace();
+					}
+*/
+					//System.out.println(j+" "+k+" [COMMIT] "+linkURL); //이거를 파일에
 				}
 			}
 		}
-		
+		System.out.println("end");
 	}
 	
 	private static void setLabelNameMap() {		
-		labelNameMap.put("atom/atom", "bug");
-		labelNameMap.put("getsentry/sentry", "\"Type%3A+Bug\"");
+		labelNameMap.put("bitcoin/bitcoin", "Bug");
+		//labelNameMap.put("getsentry/sentry", "\"Type%3A+Bug\"");
 		
 	}
 
